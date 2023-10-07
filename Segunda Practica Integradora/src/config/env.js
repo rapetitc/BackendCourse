4c9cfea9c64d5cf290b29044af1d6b2a38e232dd
Ruben Petit <rapetitc@gmail.com>
2023-10-07 03:25:54 -0300

    Segunda Practica Integradora

--- Segunda Practica Integradora/src/config/env.js
+++ Segunda Practica Integradora/src/config/env.js
@@ -5,10 +5,10 @@ export const PORT = process.env.PORT
 export const MONGODB_DBNAME = process.env.MONGODB_DBNAME
 export const MONGODB_USERNAME = process.env.MONGODB_USERNAME
 export const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD
-export const MONGODB_URL = 'mongodb://127.0.0.1:27017/ecommerce'//`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.duddyrx.mongodb.net/${MONGODB_DBNAME}`
+export const MONGODB_URL = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.duddyrx.mongodb.net/${MONGODB_DBNAME}`
 
 export const COOKIE_SECRET_KEY = process.env.COOKIE_SECRET_KEY
-export const JWT_SECRET_KEY = 'JWT_SECRET_KEY'//process.env.JWT_SECRET_KEY
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 
 export const GITHUB_APP_ID = process.env.GITHUB_APP_ID
 export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID

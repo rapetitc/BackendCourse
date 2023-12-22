0badfb3412e1d2f37db9f0b9212d2f234b05c9e2
Ruben Petit <rapetitc@gmail.com>
2023-12-22 11:12:27 -0300

    Desafio Entregable 18 - Cuarta Practica Integradora

--- Desafio Entregable 18 - Cuarta Practica Integradora/src/config/CLI.js
+++ Desafio Entregable 18 - Cuarta Practica Integradora/src/config/CLI.js
@@ -4,7 +4,7 @@ const useCLI = () => {
   const program = new Command();
 
   program.name("CLI App").description("A CLI for starting up the app");
-  program.requiredOption("-m, --mode [mode]", "Iniatiate mode of the app", "production");
+  program.requiredOption("-m, --mode [mode]", "Mode of the app", "prod");
   program.parse();
 
   return program.opts();

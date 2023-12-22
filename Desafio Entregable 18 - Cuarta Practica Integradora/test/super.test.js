5158b5a067b1897049384d05716d9460ce1dab9f
Ruben Petit <rapetitc@gmail.com>
2023-12-22 11:18:11 -0300

    Desafio Entregable 18 - Cuarta Practica Integradora

--- Desafio Entregable 18 - Cuarta Practica Integradora/test/super.test.js
+++
@@ -1,7 +0,0 @@
-import supertest from 'supertest'
-import chai from 'chai'
-import { faker } from '@faker-js/faker';
-
-const request = supertest('http://localhost:8080')
-const expect = chai.expect
-

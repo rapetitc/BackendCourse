673604c833a94dd675c97f659e66a6a4af045536
Ruben Petit <rapetitc@gmail.com>
2024-01-03 20:21:45 -0300

    Desafio Entregable 19 - Proyecto Final

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 
 export default cors({
-  origin: "https://backendcourse-production.up.railway.app/",
+  origin: "https://backendcourse-production.up.railway.app",
   credentials: true,
 });
